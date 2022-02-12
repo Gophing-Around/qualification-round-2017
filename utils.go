66e6a69c47af7d851a30fa189afb8aaa2dc4a437
Federico Maggi <federico.maggi@mia-platform.eu>
2022-02-12 15:16:25 +0100

    split functions

--- utils.go
+++ utils.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io/ioutil"
 	"strconv"
+	"strings"
 )
 
 func readFile(source string) string {
@@ -21,34 +22,39 @@ func toint(num string) int {
 	return res
 }
 
+func splitNewLines(rawContent string) []string {
+	return strings.Split(rawContent, "\n")
+}
+
+func splitSpaces(lineContent string) []string {
+	return strings.Split(lineContent, " ")
+}
 
 /* useful pieces of code:
 
-	***** [1] take input set and split in lines *****
-	inputLines := strings.Split(inputSet, "\n")
+***** [1] take input set and split in lines *****
+inputLines := strings.Split(inputSet, "\n")
 
 
-	***** [2] take one line and split values with space *****
-	line := inputLines[0]
-	values := strings.Split(line, " ")
+***** [2] take one line and split values with space *****
+line := inputLines[0]
+values := strings.Split(line, " ")
 
 
-	***** [3] sorting *****
-	sort.Slice(arrayOfValues, func(i, j int) bool {
-		valueA := arrayOfValues[i]
-		valueB := arrayOfValues[j]
-		return valueA.score > valueB.score
-	})
+***** [3] sorting *****
+sort.Slice(arrayOfValues, func(i, j int) bool {
+	valueA := arrayOfValues[i]
+	valueB := arrayOfValues[j]
+	return valueA.score > valueB.score
+})
 
 
-	***** [4] Join array of STRINGS (not integers or others) *****
-	strings.Join(arrayOfStrings, " ")
+***** [4] Join array of STRINGS (not integers or others) *****
+strings.Join(arrayOfStrings, " ")
 
-	
-	***** [5] Format output string with Sprintf *****
-	output := ""
-	output += fmt.Sprintf("%s %d\n", stringValue, intValue)
+
+***** [5] Format output string with Sprintf *****
+output := ""
+output += fmt.Sprintf("%s %d\n", stringValue, intValue)
 
 */
-
-
