d5544b899f2555d07158aa573507351459865e5f
Federico Maggi <federico.maggi@mia-platform.eu>
2022-02-12 18:17:37 +0100

    splitted algorithm

--- algorithm.go
+++ algorithm.go
@@ -44,6 +44,7 @@ func algorithm(
 
 		groupedPotentialRequests := make([]*GroupedPotentialRequest, 0)
 		groupedPotentialRequestsMap := make(map[int]*GroupedPotentialRequest)
+
 		for _, potRequest := range server.potentialRequests {
 			videoId := potRequest.videoId
 
@@ -108,61 +109,96 @@ func algorithm(
 
 			server.allocatedVideoMap[potentialRequest.videoId] = true
 		}
+	}
 
-		// sort.Slice(server.potentialRequests, func(a int, b int) bool {
-		// 	requestA := server.potentialRequests[a]
-		// 	requestB := server.potentialRequests[b]
+	return 42
+}
 
-		// 	dcLatencyA := requestA.endpoint.dcLatency
-		// 	dcLatencyB := requestB.endpoint.dcLatency
+func algorithm2(
+	Config Config,
+	videos []*Video,
+	serversMap mapOfServers,
+	endpointsMap mapOfEndpoints,
+	requestList []*RequestGroup,
+	serversList []*CacheServer,
+) int {
 
-		// 	cacheALatency := server.endpointLatencyMap[requestA.endpointId]
-		// 	cacheBLatency := server.endpointLatencyMap[requestB.endpointId]
+	globalAllocationMap := make(map[int]int)
 
-		// 	gainA := dcLatencyA - cacheALatency
-		// 	gainB := dcLatencyB - cacheBLatency
+	sort.Slice(serversList, func(a, b int) bool {
+		serverA := serversList[a]
+		serverB := serversList[b]
 
-		// 	videoASize := requestA.video.size
-		// 	videoBSize := requestB.video.size
+		potReqA := 0
+		for _, req := range serverA.potentialRequests {
+			potReqA += req.nRequests
+		}
 
-		// 	return gainA*requestA.nRequests/videoASize > gainB*requestB.nRequests/videoBSize
-		// })
+		potReqB := 0
+		for _, req := range serverB.potentialRequests {
+			potReqB += req.nRequests
+		}
 
-		// for _, potentialRequest := range server.potentialRequests {
-		// 	video := potentialRequest.video
-		// 	videoAlreadyAllocatedInServer := server.allocatedVideoMap[potentialRequest.videoId]
-		// 	_, videoAlreadyAllocatedGlobally := globalAllocationMap[potentialRequest.videoId]
+		return potReqA > potReqB
+	})
 
-		// 	if videoAlreadyAllocatedInServer || videoTotlaSize+video.size > server.serverCapacity {
-		// 		continue
-		// 	}
+	for _, server := range serversList {
+		videoTotlaSize := 0
 
-		// 	cacheALatency := server.endpointLatencyMap[potentialRequest.endpointId]
-		// 	gainedLatencyPerRequest := (potentialRequest.endpoint.dcLatency - cacheALatency) * potentialRequest.nRequests
-		// 	if videoAlreadyAllocatedGlobally { // && globalLatencyPerRequest < gainedLatencyPerRequest {
-		// 		continue
-		// 	}
+		sort.Slice(server.potentialRequests, func(a int, b int) bool {
+			requestA := server.potentialRequests[a]
+			requestB := server.potentialRequests[b]
 
-		// 	server.allocatedVideos = append(server.allocatedVideos, fmt.Sprintf("%d", video.id))
-		// 	videoTotlaSize += video.size
+			dcLatencyA := requestA.endpoint.dcLatency
+			dcLatencyB := requestB.endpoint.dcLatency
 
-		// 	server.allocatedVideoMap[potentialRequest.videoId] = true
-		// 	globalAllocationMap[potentialRequest.videoId] = gainedLatencyPerRequest
-		// }
+			cacheALatency := server.endpointLatencyMap[requestA.endpointId]
+			cacheBLatency := server.endpointLatencyMap[requestB.endpointId]
 
-		// for _, potentialRequest := range server.potentialRequests {
-		// 	video := potentialRequest.video
-		// 	videoAlreadyAllocatedInServer := server.allocatedVideoMap[potentialRequest.videoId]
+			gainA := dcLatencyA - cacheALatency
+			gainB := dcLatencyB - cacheBLatency
 
-		// 	if videoAlreadyAllocatedInServer || videoTotlaSize+video.size > server.serverCapacity {
-		// 		continue
-		// 	}
+			videoASize := requestA.video.size
+			videoBSize := requestB.video.size
 
-		// 	server.allocatedVideos = append(server.allocatedVideos, fmt.Sprintf("%d", video.id))
-		// 	videoTotlaSize += video.size
+			return gainA*requestA.nRequests/videoASize > gainB*requestB.nRequests/videoBSize
+		})
 
-		// 	server.allocatedVideoMap[potentialRequest.videoId] = true
-		// }
+		for _, potentialRequest := range server.potentialRequests {
+			video := potentialRequest.video
+			videoAlreadyAllocatedInServer := server.allocatedVideoMap[potentialRequest.videoId]
+			_, videoAlreadyAllocatedGlobally := globalAllocationMap[potentialRequest.videoId]
+
+			if videoAlreadyAllocatedInServer || videoTotlaSize+video.size > server.serverCapacity {
+				continue
+			}
+
+			cacheALatency := server.endpointLatencyMap[potentialRequest.endpointId]
+			gainedLatencyPerRequest := (potentialRequest.endpoint.dcLatency - cacheALatency) * potentialRequest.nRequests
+			if videoAlreadyAllocatedGlobally { // && globalLatencyPerRequest < gainedLatencyPerRequest {
+				continue
+			}
+
+			server.allocatedVideos = append(server.allocatedVideos, fmt.Sprintf("%d", video.id))
+			videoTotlaSize += video.size
+
+			server.allocatedVideoMap[potentialRequest.videoId] = true
+			globalAllocationMap[potentialRequest.videoId] = gainedLatencyPerRequest
+		}
+
+		for _, potentialRequest := range server.potentialRequests {
+			video := potentialRequest.video
+			videoAlreadyAllocatedInServer := server.allocatedVideoMap[potentialRequest.videoId]
+
+			if videoAlreadyAllocatedInServer || videoTotlaSize+video.size > server.serverCapacity {
+				continue
+			}
+
+			server.allocatedVideos = append(server.allocatedVideos, fmt.Sprintf("%d", video.id))
+			videoTotlaSize += video.size
+
+			server.allocatedVideoMap[potentialRequest.videoId] = true
+		}
 	}
 
 	return 42
